f5e2d43e134c17a648efce0063ccb557f3c706f2
abeer26omar <abeer.omar504@gmail.com>
2022-07-12 14:39:59 +0200

    add user model&form control

---
+++ src/app/user.model.ts
@@ -0,0 +1,31 @@
+export class User{
+    constructor(
+        public name: string, 
+        public id: string, 
+        private _token: string, 
+        private _tokenExpirationDate?: Date){}
+
+        get token(){
+            if(!this._tokenExpirationDate || new Date() > this._tokenExpirationDate){
+                return null
+            }
+            return this._token;
+        }
+}
+
+export interface Register{
+    data: {
+        user: {
+            name: string,
+            gender: string,
+            birth_date: string,
+            phone: string,
+            otp: number,
+            id: string,
+            image_url: string,
+            cover_url: string,
+            expiresIn: string,
+        },
+        token: string
+    }      
+}

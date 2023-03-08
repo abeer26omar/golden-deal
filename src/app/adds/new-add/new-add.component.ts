91e1e411b8e8ca5098e7e465195613b64bb91e22
abeer26omar <abeer.omar504@gmail.com>
2023-03-08 17:29:24 +0200

    modify: add products

--- src/app/adds/new-add/new-add.component.ts
+++ src/app/adds/new-add/new-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -7,6 +7,7 @@ import { ProductsRequestService } from 'src/app/services/products-request.servic
 import { HttpErrorResponse } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
 import { MacPrefixService } from 'src/app/services/mac-prefix.service';
+import { NgOtpInputComponent, NgOtpInputConfig } from 'ng-otp-input';
 
 declare var window: any;
 
@@ -25,7 +26,10 @@ export class NewAddComponent implements OnInit {
   images : string[] = [];
   filtersArr: any = [];
   error: string = '';
+  errorAdd: string = '';
+  error_CarPlate: string = '';
   load: boolean = false;
+  error_CarPlate_bool: boolean = false;
   file!: File;
   imgSrc1: any;
   imgSrc2: any;
@@ -47,7 +51,7 @@ export class NewAddComponent implements OnInit {
   }
   filterOptions :any= [];
   valueArr: any = [];
-  submitted = false;
+  // submitted = false;
   catergoryId!:number;
   ownerId = localStorage.getItem('userId');
   NewProductRes: NewProduct = {
@@ -56,32 +60,92 @@ export class NewAddComponent implements OnInit {
       msg: ''
     }
   }
- 
+  plates_chars: any = [
+    { char: 'أ' , trans: 'A' },
+    { char: 'ب' , trans: 'B' },
+    { char: 'ح' , trans: 'J '},
+    { char: 'د' , trans: 'D' },
+    { char: 'ر' , trans: 'R' },
+    { char: 'س' , trans: 'S' },
+    { char: 'ص' , trans: 'X' },
+    { char: 'ط' , trans: 'T' },
+    { char: 'ع' , trans: 'E' },
+    { char: 'ق' , trans: 'G' },
+    { char: 'ك' , trans: 'K' },
+    { char: 'ل' , trans: 'L' },
+    { char: 'م' , trans: 'Z' },
+    { char: 'ن' , trans: 'N' },
+    { char: 'هـ' , trans: 'H' },
+    { char: 'و' , trans: 'U' },
+    { char: 'ى' , trans: 'V' }
+  ]
+  // otp card chars
+  @ViewChild(NgOtpInputComponent, { static: false}) ngOtpInput!:NgOtpInputComponent;
+  subOtp: boolean = true;
+  otp!: string ;
+  showOtpComponent = true;
+  configAr:NgOtpInputConfig = {
+    allowNumbersOnly: false,
+    length: 3,
+    allowKeyCodes: [],
+    isPasswordInput: false,
+    disableAutoFocus: true,
+    placeholder: '',
+    containerStyles:{
+      'direction': 'rtl'
+    },
+    inputStyles:{
+      'margin-left': '8px',
+      'margin-right': '0px'
+    }
+  };
+  onOtpChange(otp:any) {
+    // this.ngOtpInput.setValue(otp);
+    console.log(otp);
+    // this.otp = otp;
+    // if(otp.length == 4){
+    //   this.subOtp = !this.subOtp;
+    // }
+  }
+  configEn:NgOtpInputConfig = {
+    allowNumbersOnly: false,
+    length: 3,
+    allowKeyCodes: [],
+    isPasswordInput: false,
+    disableAutoFocus: true,
+    placeholder: '',
+    containerStyles:{
+      'direction': 'rtl'
+    },
+    inputStyles:{
+      'margin-left': '8px',
+      'margin-right': '0px'
+    }
+  }
   private categorySub : Subscription = new Subscription;
   private filterSub : Subscription = new Subscription;
   private sendSub: Subscription = new Subscription;
   constructor(private httpService: ProductsRequestService,
     private router: Router,
-    private macService: MacPrefixService) { }
+    private macService: MacPrefixService) {
+      // this.ngOtpInput.otpForm.disable();
+     }
     
     myForm = new FormGroup({
-      negotiable: new FormControl(''),
       seller_phone: new FormControl('', [Validators.required]),
       productCategory: new FormControl('', [Validators.required]),
       name: new FormControl('', [Validators.required]),
       price: new FormControl('', [Validators.required]),
-      about_seller: new FormControl(''),
-      delivery_notes: new FormControl(''),
       desc: new FormControl('', [Validators.required]),
-      materials: new FormControl('', [Validators.required]),
       owner_id: new FormControl(this.ownerId),
+      negotiable: new FormControl(''),
       category_id: new FormControl(''),
       ownership_image: new FormControl(''),
-      product_image_1: new FormControl(''),
-      product_image_2: new FormControl(''),
-      product_image_3: new FormControl(''),
-      product_image_4: new FormControl(''),
-      product_image_5: new FormControl('')
+      product_image_1: new FormControl('',Validators.required),
+      product_image_2: new FormControl('',Validators.required),
+      product_image_3: new FormControl('',Validators.required),
+      product_image_4: new FormControl('',Validators.required),
+      product_image_5: new FormControl('',Validators.required),
     })
     get f(){
       return this.myForm.controls;
@@ -101,68 +165,102 @@ export class NewAddComponent implements OnInit {
   onNegotiable(){
     if(this.negotiable == false){
       this.negotiable = true;
+      this.f['negotiable'].setValue(1)
       this.f['price'].disable()
+      this.f['price'].setValue('0')
     }else{
       this.negotiable = false;
+      this.f['negotiable'].setValue(0)
       this.f['price'].enable()
+      this.f['price'].setValue('')
     }
-    console.log(this.negotiable);
   }
-  onFileChange1(event:any) {
+  onFileChange(key: number,event: any){
     let reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
     this.file =<File>event.target.files[0];
-    reader.onload = ()=>{
-      this.imgSrc1 = reader.result;
-      this.images.splice(0,1,this.imgSrc1)
+    switch(key){
+      case 1:
+        reader.onload = ()=>{
+          this.imgSrc1 = reader.result;
+        }
+        this.myForm.get('product_image_1')?.patchValue(this.file,this.file.name);
+        break;
+      case 2:
+        reader.onload = ()=>{
+          this.imgSrc2 = reader.result;
+        }
+        this.myForm.get('product_image_2')?.patchValue(this.file,this.file.name);
+      break;
+      case 3:
+        reader.onload = ()=>{
+          this.imgSrc3 = reader.result;
+        }
+        this.myForm.get('product_image_3')?.patchValue(this.file,this.file.name);
+      break;
+      case 4:
+        reader.onload = ()=>{
+          this.imgSrc4 = reader.result;
+        }
+        this.myForm.get('product_image_4')?.patchValue(this.file,this.file.name);
+      break;
+      case 5:
+        reader.onload = ()=>{
+          this.imgSrc5 = reader.result;
+        }
+        this.myForm.get('product_image_5')?.patchValue(this.file,this.file.name);
+      break;
     }
-    this.myForm.get('product_image_1')?.patchValue(this.file,this.file.name);
-  }
-  onFileChange2(event:any) {
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    this.file =<File>event.target.files[0];
-    reader.onload = ()=>{
-      this.imgSrc2 = reader.result;
-      this.images.splice(2,1,this.imgSrc2)
-    }
-    this.myForm.get('product_image_2')?.patchValue(this.file,this.file.name);
-  }
-  onFileChange3(event:any) {
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    this.file =<File>event.target.files[0];
-    reader.onload = ()=>{
-      this.imgSrc3 = reader.result;
-      this.images.splice(1,1,this.imgSrc3)
-    }    
-    this.myForm.get('product_image_3')?.patchValue(this.file,this.file.name);
-  }
-  onFileChange4(event:any) {
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    this.file =<File>event.target.files[0];
-    reader.onload = ()=>{
-      this.imgSrc4 = reader.result;
-      this.images.splice(2,1,this.imgSrc4)
-    }
-    this.myForm.get('product_image_4')?.patchValue(this.file,this.file.name);
-  }
-  onFileChange5(event:any) {
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    this.file =<File>event.target.files[0];
-    reader.onload = ()=>{
-      this.imgSrc5 = reader.result;
-      this.images.splice(2,1,this.imgSrc5)
-    }
-    this.myForm.get('product_image_5')?.patchValue(this.file,this.file.name);
   }
   onFile(event:any) {
     let reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
     this.file =<File>event.target.files[0];
     this.ownership = this.file.name
+  }
+  getCardChar(key: number,value: any){
+    let plate_char = value.target.value;
+    let trans_values: any = [], trans_value:any;
+    this.error_CarPlate = '';
+    switch(key){
+      case 1: 
+        this.plates_chars.forEach((char: any) => {
+          if(char.char == plate_char){
+            this.error_CarPlate = '';
+            trans_values.push(char.char)
+            return trans_value = char.trans;
+          }else{
+            this.error_CarPlate = 'حروف اللوحه يجب ان تكون ضمن هذه المجموعه [أ-ب-ح-د-ر-س-ص-ط-ع-ق-ك-ل-م-ن-ه-و-ى]';          
+          }
+        });
+        this.myForm.get('plate_chars_en_1')?.setValue(trans_value)
+      break;
+      case 2:
+        this.plates_chars.forEach((char: any) => {
+          if(char.char == plate_char){
+            this.error_CarPlate = '';
+            trans_values.push(char.char)
+            return trans_value = char.trans;
+          }else{
+            this.error_CarPlate = 'حروف اللوحه يجب ان تكون ضمن هذه المجموعه [أ-ب-ح-د-ر-س-ص-ط-ع-ق-ك-ل-م-ن-ه-و-ى]';
+          }
+        });
+        this.myForm.get('plate_chars_en_2')?.setValue(trans_value)
+      break;
+      case 3:
+        this.plates_chars.forEach((char: any) => {
+          if(char.char == plate_char){
+            this.error_CarPlate = '';
+            trans_values.push(char.char)
+            return trans_value = char.trans;
+          }else{
+            this.error_CarPlate = 'حروف اللوحه يجب ان تكون ضمن هذه المجموعه [أ-ب-ح-د-ر-س-ص-ط-ع-ق-ك-ل-م-ن-ه-و-ى]';
+          }
+        });
+        this.myForm.get('plate_chars_en_3')?.setValue(trans_value)
+      break;
+    }
+    this.myForm.get('plate_chars_filter_6')?.setValue(trans_values)
   }
   getValue(event: any,slug_name: any){
     let final_values:any = {}
@@ -211,9 +309,25 @@ export class NewAddComponent implements OnInit {
   getCategoryFilter(categoryName: any){ 
     if(categoryName == 'car_plates'){
       this.defaultImage_add = false;
+      this.myForm.removeControl('product_image_1');
+      this.myForm.removeControl('product_image_2');
+      this.myForm.removeControl('product_image_3');
+      this.myForm.removeControl('product_image_4');
+      this.myForm.removeControl('product_image_5');
+      this.myForm.addControl('plate_chars_en_1', new FormControl(''));
+      this.myForm.addControl('plate_chars_en_2', new FormControl(''));
+      this.myForm.addControl('plate_chars_en_3', new FormControl(''));
     }
     else{
       this.defaultImage_add = true;
+      this.myForm.addControl('product_image_1', new FormControl('',Validators.required));
+      this.myForm.addControl('product_image_2', new FormControl('',Validators.required));
+      this.myForm.addControl('product_image_3', new FormControl('',Validators.required));
+      this.myForm.addControl('product_image_4', new FormControl('',Validators.required));
+      this.myForm.addControl('product_image_5', new FormControl('',Validators.required));
+      this.myForm.removeControl('plate_chars_en_1');
+      this.myForm.removeControl('plate_chars_en_2');
+      this.myForm.removeControl('plate_chars_en_3');
     }
     this.categories.forEach(ele=>{
       if(ele.slug == categoryName){
@@ -223,7 +337,9 @@ export class NewAddComponent implements OnInit {
     })
     this.filterSub = this.httpService.getCategoryFilters(categoryName).subscribe({
       next: (res: CategoryFilter)=>{
-        this.filters = res;        
+        this.filters = res;
+        console.log(this.filters);
+                
         this.filters.data.filters.forEach(filter=>{
           this.newFormControl = filter.slug_name;
           this.myForm.addControl(filter.slug_name, new FormControl('')) 
@@ -241,28 +357,20 @@ export class NewAddComponent implements OnInit {
     })
   }
   submit(){
-    this.submitted = true;    
-    // if((this.myForm.get('agrement')?.invalid)){
-    //   return;
-    // } 
-    // else{
-      // if(this.step == 0){
-      //   this.step = this.step + 1;
-      //   this.submitted = false;    
-      // }
+    if(this.myForm.valid){
       this.getProductCategoryId();   
       const formData = new FormData();
       for (const field in this.myForm.controls) {
         formData.append(field, this.myForm.controls[field].value);
       }
-      if(this.myForm.valid){
         this.load = true;
-      this.sendSub = this.httpService.http.post<NewProduct>(`${env.api_url}/products/store-new-product`,
+        this.sendSub = this.httpService.http.post<NewProduct>(`${env.api_url}/products/store-new-product`,
           formData,
         this.httpService.httpOptions)
         .subscribe({
           next: (res: NewProduct)=>{
             this.load = false;
+            this.errorAdd = ''
             this.NewProductRes = res;
             this.modelSuccessNewProduct.show();
           },
@@ -277,6 +385,8 @@ export class NewAddComponent implements OnInit {
           }
         })   
       } else{
+        console.log(this.myForm);
+        this.errorAdd = 'يجب ادخال البيانات'
         return;
       }
     // }

9b9974260535dc0b8f095e9025331db54bbb576c
abeer26omar <abeer.omar504@gmail.com>
2023-04-06 14:58:51 +0200

    change api domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://deal.qbizns.com/api',
+  api_url: 'https://focused-merkle.185-92-223-5.plesk.page/golden-deal/public/api',
   // socket_url: 'http://95.179.148.143:3000'
   // socket_url: 'http://localhost:3000'
   socket_url: 'https://golden-deal-socket.herokuapp.com/'

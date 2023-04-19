8b872e914dc3e8b3ae65f84af635f70d75adeb26
abeer26omar <abeer.omar504@gmail.com>
2023-04-19 15:45:18 +0200

    fix: error response

--- src/app/response-modal-error/response-modal-error.component.ts
+++ src/app/response-modal-error/response-modal-error.component.ts
@@ -10,16 +10,15 @@ export class ResponseModalErrorComponent implements OnInit {
   message: string = '';
   constructor(@Inject(MAT_DIALOG_DATA) public data:any,
   public dialogRef: MatDialogRef<MatDialogClose>) {   
-    console.log(data.response);
     
     if(data.response.error.errors){
       Object.values(data.response.error.errors).forEach((e: any)=>{
         this.message = e
       });
     }else if(data.response.error){
-      this.message = data.response.error.data
+      this.message = data.response.error.data || data.response.error.message
     }else{
-      this.message = data.response.message
+      this.message = data.response.message      
     }
     if(data.response.statusText == "Unknown Error"){
       this.message = data.response.statusText
